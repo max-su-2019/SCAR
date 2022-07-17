a6ad8fbf44a8b88327ccf9d8b7531928541b055d
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2022-07-17 11:57:26 +0800

    Update to v0.91

--- src/Hook_MainUpdate.h
+++ src/Hook_MainUpdate.h
@@ -11,13 +11,12 @@ namespace SCAR
 			static std::uint32_t baseID = 36544, offset = 0x160;  //Anniversary Edition
 #else
 			static std::uint32_t baseID = 35551, offset = 0x11F;  //Special Edition
-		};
 #endif
 			REL::Relocation<uintptr_t> hook{ REL::ID(baseID) };
 			auto& trampoline = SKSE::GetTrampoline();
 			SKSE::AllocTrampoline(1 << 4);
 			_Update = trampoline.write_call<5>(hook.address() + offset, Update);
-		}
+		};
 
 	private:
 		static void Update(RE::Main* a_this, float a2);
