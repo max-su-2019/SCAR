8588663d4fb506c39bd59d0e4c19fc3f6bbbeb5f
Alan Tse <alandtse@gmail.com>
2023-04-17 21:47:26 -0700

    Port to VR

--- src/LoadGame.cpp
+++ src/LoadGame.cpp
@@ -30,6 +30,10 @@ namespace SCAR
 
 			if (dataHandler->settings->enableDebugOverlay.get_data()) {
 				//DebugOverlayMenu
+				if (REL::Module::IsVR()) {
+					WARN("DebugOverlay not supported in VR; ignoring setting");
+					return;
+				}
 				DebugOverlayMenu::Register();
 				MainUpdateHook::Hook();
 				DEBUG("Enable Debug Overlay!");
